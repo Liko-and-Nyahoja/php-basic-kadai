7ca4b4d5dc9fde3f4d8371147280ec3c5a47af9a
Akiho Tabata <ackee.gamer1998@gmail.com>
2024-11-21 19:40:32 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        class Food {
+            private $name;
+            private $price;
+
+            public function __construct($name, $price) {
+                $this->name = $name;
+                $this->price = $price;
+            }
+
+            public function show_price() {
+                echo $this->price . "<br>";
+            }
+        }
+
+        class Animal {
+            private $name;
+            private $height;
+            private $weight;
+
+            public function __construct($name, $height, $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+
+            public function show_height() {
+                echo $this->height . "<br>";
+            }
+        }
+
+        // Foodクラスのインスタンスを作成
+        $food = new Food("potato", 250);
+        $animal = new Animal("dog", 60, 5000);
+
+        // インスタンスの内容を出力
+        echo (print_r($food, true)) . "<br>";
+        echo (print_r($animal, true)) . "<br>";
+
+        // メソッドを実行
+        $food->show_price();
+        $animal->show_height();
+
+        ?>
+    </p>
+</body>
+        
+</html>
